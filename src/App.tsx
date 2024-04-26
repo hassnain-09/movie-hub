55d66a3a4718664842103d7a4353a9b70aff04f4
alikhan_09 <reachhassnainali@gmail.com>
2024-04-26 11:36:20 +0500

    Adding Chakra UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import "./App.css";
+import { Button, ButtonGroup } from "@chakra-ui/react";
 
 function App() {
   return (
     <>
+      <Button>Press</Button>
       <h1>Umar Khan</h1>
     </>
   );
